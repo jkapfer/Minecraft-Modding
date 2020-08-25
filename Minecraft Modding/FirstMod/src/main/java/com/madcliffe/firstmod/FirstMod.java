bef1ee0b0c61df27a0a8d0f3e9cbbfb9597fb862
Jacob Kapfer <45276998+jkapfer@users.noreply.github.com>
2020-08-25 18:59:00 -0500

    readded item. (game is working)

--- Minecraft Modding/FirstMod/src/main/java/com/madcliffe/firstmod/FirstMod.java
+++ Minecraft Modding/FirstMod/src/main/java/com/madcliffe/firstmod/FirstMod.java
@@ -1,5 +1,6 @@
 package com.madcliffe.firstmod;
 
+import com.madcliffe.firstmod.util.RegistryHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -13,6 +14,7 @@ import org.apache.logging.log4j.Logger;
 public class FirstMod {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "firstmod";
 
     public FirstMod() {
         // Register the setup method for modloading
@@ -20,6 +22,8 @@ public class FirstMod {
 
         // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        RegistryHandler.init();
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
