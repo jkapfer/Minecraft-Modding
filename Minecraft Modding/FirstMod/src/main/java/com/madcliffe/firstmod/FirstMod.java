06ce60481e1a7e5c844d55d6f00eb4a74343444a
Jacob Kapfer <45276998+jkapfer@users.noreply.github.com>
2020-08-25 12:07:31 -0500

    Revert "Creating folders and code to add first item to game"
    This reverts commit 6862e8ef4629d5bacac49c6925586d8841dedf6a.

--- Minecraft Modding/FirstMod/src/main/java/com/madcliffe/firstmod/FirstMod.java
+++ Minecraft Modding/FirstMod/src/main/java/com/madcliffe/firstmod/FirstMod.java
@@ -1,6 +1,5 @@
 package com.madcliffe.firstmod;
 
-import com.madcliffe.firstmod.util.RegistryHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -14,8 +13,6 @@ import org.apache.logging.log4j.Logger;
 public class FirstMod {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
-    public static final String MOD_ID = "firstmod";
-
 
     public FirstMod() {
         // Register the setup method for modloading
@@ -23,9 +20,6 @@ public class FirstMod {
 
         // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
-
-        //calling init to main class
-        RegistryHandler.init();
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
