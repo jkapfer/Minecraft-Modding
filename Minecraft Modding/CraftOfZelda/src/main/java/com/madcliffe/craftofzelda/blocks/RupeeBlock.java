d411ea196569d4b819907ba83c059230d4e8f836
Jacob Kapfer <45276998+jkapfer@users.noreply.github.com>
2020-08-26 08:39:50 -0500

    updated behavior of Rupee Block

--- Minecraft Modding/CraftOfZelda/src/main/java/com/madcliffe/craftofzelda/blocks/RupeeBlock.java
+++ Minecraft Modding/CraftOfZelda/src/main/java/com/madcliffe/craftofzelda/blocks/RupeeBlock.java
@@ -10,7 +10,7 @@ public class RupeeBlock extends Block {
     public RupeeBlock() {
         super(Block.Properties.create(Material.IRON)
                 .hardnessAndResistance(5.0f, 6.0f)
-                .sound(SoundType.GLASS)
+                .sound(SoundType.METAL)
                 .harvestLevel(2)
                 .harvestTool(ToolType.PICKAXE)
         );
